02f9d6982766a48587d2a08817ac1f293784c711
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2019-11-30 15:37:59 -0500

    Add color

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const core = require('@actions/core');
 const cowsay = require("cowsay");
+const colors = require("colors");
 
 async function run() {
   const text = core.getInput('text');
 
-  console.log(cowsay.say({ text : text }));
+  let color = core.getInput('color');
+
+  if (color === undefined) {
+    color = "white"
+  }
+
+  colors.setTheme({
+    silly: color,
+  });
+
+  console.log(colors.silly(cowsay.say({ text : text })));
 }
 
 run()
